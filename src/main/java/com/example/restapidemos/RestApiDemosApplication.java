b0979a02d0d914ec4008a2d69ef36ac5880b59a3
Janusz <janusz.siwik10@gmail.com>
2024-01-15 21:40:05 +0100

    initial commit

---
+++ src/main/java/com/example/restapidemos/RestApiDemosApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapidemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapidemos/RestApiDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapidemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
